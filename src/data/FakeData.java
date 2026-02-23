f6ceef69f742673c3c51d555767dfca2f075afeb
Leyla P. <liya1998@list.ru>
2026-02-23 18:08:45 +0600

    Added example books to fill the html pages

--- src/data/FakeData.java
+++ src/data/FakeData.java
@@ -9,7 +9,7 @@ public class FakeData {
     public List<Book> getBooks() {
         return List.of(
                 new Book(1, "Clean Code", "Robert Martin", "/img/clean.jpg", null, false),
-                new Book(2, "Effective Java", "Joshua Bloch", "/img/java.jpg", "Айбек", true),
+                new Book(2, "Effective Java", "Joshua Bloch", "/img/java.jpg", "Лейла", true),
                 new Book(3, "Design Patterns", "GoF", "/img/patterns.jpg", null, false)
         );
     }

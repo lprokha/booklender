4ce0e9f578529b21a1e37a1f5e3678ab6a09159d
Leyla P. <liya1998@list.ru>
2026-02-23 18:31:31 +0600

    Added lists of current and past books

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,12 +1,19 @@
 package model;
 
+import java.util.List;
+
 public class Employee {
     private int id;
     private String fullName;
+    private List<Book> currentBooks;
+    private List<Book> pastBooks;
 
-    public Employee(int id, String fullName) {
+
+    public Employee(int id, String fullName, List<Book> currentBooks, List<Book> pastBooks) {
         this.id = id;
         this.fullName = fullName;
+        this.currentBooks = currentBooks;
+        this.pastBooks = pastBooks;
     }
 
     public int getId() {
@@ -15,5 +22,13 @@ public class Employee {
 
     public String getFullName() {
         return fullName;
+    }
+
+    public List<Book> getCurrentBooks() {
+        return currentBooks;
+    }
+
+    public List<Book> getPastBooks() {
+        return pastBooks;
     }
 }

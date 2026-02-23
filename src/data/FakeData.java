97292aa6fa097123a4f5fee9efd62be2261014ee
Leyla P. <liya1998@list.ru>
2026-02-23 18:31:09 +0600

    Added employee data and a new book

--- src/data/FakeData.java
+++ src/data/FakeData.java
@@ -1,6 +1,7 @@
 package data;
 
 import model.Book;
+import model.Employee;
 
 import java.util.List;
 
@@ -10,7 +11,8 @@ public class FakeData {
         return List.of(
                 new Book(1, "Clean Code", "Robert Martin", "/img/clean.jpg", null, false),
                 new Book(2, "Effective Java", "Joshua Bloch", "/img/java.jpg", "Лейла", true),
-                new Book(3, "Design Patterns", "GoF", "/img/patterns.jpg", null, false)
+                new Book(3, "Design Patterns", "GoF", "/img/patterns.jpg", null, false),
+                new Book(4, "Refactoring", "Martin Fowler", "/img/refactoring.jpg", "Лейла", true)
         );
     }
 
@@ -20,5 +22,23 @@ public class FakeData {
                 .filter(b -> b.getId() == id)
                 .findFirst()
                 .orElse(null);
+    }
+
+    public Employee getEmployee() {
+        return new Employee(1, "Лейла Прохивская", getEmployeeCurrentBooks(), getEmployeePastBooks());
+    }
+
+    public List<Book> getEmployeeCurrentBooks() {
+        return List.of(
+                new Book(2, "Effective Java", "Joshua Bloch", "/img/java.jpg", "Лейла", true),
+                new Book(4, "Refactoring", "Martin Fowler", "/img/refactoring.jpg", "Лейла", true)
+        );
+    }
+
+    public List<Book> getEmployeePastBooks() {
+        return List.of(
+                new Book(1, "Clean Code", "Robert Martin", "/img/clean.jpg", null, false),
+                new Book(3, "Design Patterns", "GoF", "/img/patterns.jpg", null, false)
+        );
     }
 }

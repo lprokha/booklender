7d7a3914b9c88c302262380bc4253736ba86abcd
Leyla P. <liya1998@list.ru>
2026-02-26 17:13:22 +0600

    Added User.java and UserStorage.java to store users data

---
+++ src/model/User.java
@@ -0,0 +1,25 @@
+package model;
+
+public class User {
+    private String email;
+    private String password;
+    private String fullName;
+
+    public User(String email, String password, String fullName) {
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
